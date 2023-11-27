a0e76833b3e850fc9d45b7aae5335cc11dd81105
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2023-11-27 16:15:38 +0300

    📝 Update tests

--- Kampus/Design project structure/test/Test.kt
+++ Kampus/Design project structure/test/Test.kt
@@ -2,34 +2,80 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.testing.*
 import kotlinx.coroutines.runBlocking
+import org.hyperskill.hstest.dynamic.DynamicTest
+import org.hyperskill.hstest.stage.StageTest
+import org.hyperskill.hstest.testcase.CheckResult
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class Test {
+class Test: StageTest<Any>() {
     private val client = HttpClient()
 
-    @Test
-    fun `Student service is responding`() = runBlocking {
-        val response = client.get("http://localhost:8000")
-        assertEquals(HttpStatusCode.OK, response.status)
+    @DynamicTest
+    fun `Student service is responding`(): CheckResult {
+        var result: CheckResult = CheckResult.correct()
+        runBlocking {
+            try {
+                val response = client.get("http://localhost:8000")
+                println(response)
+                if (response.status != HttpStatusCode.OK)
+                    result = CheckResult.wrong("The student service is not responding")
+            } catch (exc: Throwable) {
+                result = CheckResult.wrong(exc.message)
+            }
+        }
+        return result
     }
 
-    @Test
-    fun `Student service returns correct string`() = runBlocking {
-        val response = client.get("http://localhost:8000")
-        assertEquals("Hello from StudentService!", response.bodyAsText())
+
+    @DynamicTest
+    fun `Student service returns correct string`(): CheckResult {
+        var result: CheckResult = CheckResult.correct()
+        runBlocking {
+            try {
+                val response = client.get("http://localhost:8000")
+                println(response)
+                if (response.bodyAsText() != "Hello from StudentService!")
+                    result = CheckResult.wrong("The student service is not responding")
+            } catch (exc: Throwable) {
+                result = CheckResult.wrong(exc.message)
+            }
+        }
+        return result
     }
 
-    @Test
-    fun `Schedule service is responding`() = runBlocking {
-        val response = client.get("http://localhost:8001")
-        assertEquals(HttpStatusCode.OK, response.status)
+
+    @DynamicTest
+    fun `Schedule service is responding`(): CheckResult {
+        var result: CheckResult = CheckResult.correct()
+        runBlocking {
+            try {
+                val response = client.get("http://localhost:8001")
+                println(response)
+                if (response.status != HttpStatusCode.OK)
+                    result = CheckResult.wrong("The student service is not responding")
+            } catch (exc: Throwable) {
+                result = CheckResult.wrong(exc.message)
+            }
+        }
+        return result
     }
 
-    @Test
-    fun `Schedule service returns correct string`() = runBlocking {
-        val response = client.get("http://localhost:8001")
-        assertEquals("Hello from ScheduleService!", response.bodyAsText())
+    @DynamicTest
+    fun `Schedule service returns correct string`(): CheckResult {
+        var result: CheckResult = CheckResult.correct()
+        runBlocking {
+            try {
+                val response = client.get("http://localhost:8001")
+                println(response)
+                if (response.bodyAsText() != "Hello from ScheduleService!")
+                    result = CheckResult.wrong("The schedule service is not responding")
+            } catch (exc: Throwable) {
+                result = CheckResult.wrong(exc.message)
+            }
+        }
+        return result
     }
 }
